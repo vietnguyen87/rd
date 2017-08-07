544fb0b893f456dad842c07d6a229fb79a4ffb81
vietnguyen87 <vietnguyen.ag@gmail.com>
2017-08-07 16:04:59 +0700

    rd

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # rd
+Rd is a Golang library for R&D team.
+
+https://github.com/vietnguyen87/rd
+
+How to install:
+
+go get github.com/vietnguyen87/rd
+
+How to use:
+
+In the example below we call BuildInnerQueriesForSearchProductNewV8 func with some params.
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"./lib"
+	"encoding/json"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var matchQuerySlice []interface{}
+	var keywordFormat string
+	var isAccentMarks bool = false
+	var isSearchInsideCategory bool = true
+	var firstTerm 		= ""
+	matchQuerySlice = lib.BuildInnerQueriesForSearchProductNewV8(matchQuerySlice, isSearchInsideCategory, keywordFormat, isAccentMarks, firstTerm)
+	mapB, _ := json.Marshal(matchQuerySlice)
+	fmt.Println(string(mapB))
+}
+
+Parameters Using
+
+matchQuerySlice []interface{}
+keywordFormat   string
+isAccentMarks bool
+isSearchInsideCategory bool
+firstTerm string
--- main.go
+++ main.go
@@ -1,114 +1,19 @@
-package rd
+package main
 
-func BuildInnerQueriesForSearchProductNewV8(matchQuerySlice []interface{}, isSearchInsideCategory bool, keywordFormat string, isAccentMarks bool, firstTerm string) []interface{} {
-	var field1 []string
-	if isSearchInsideCategory {
-		field1 = []string{"Sku_user"}
-	} else {
-		field1 = []string{"Category_name_suggest", "Sku_user"}
-	}
-	var field2 = []string{"Name"}
-	var keyWordFinal string
-	keyWordFinal = keywordFormat
+import (
+	"fmt"
+	"runtime"
+	"./lib"
+	"encoding/json"
+)
 
-	for i := 0; i < len(field1); i++ {
-		if isAccentMarks == true{
-			matchQuerySlice = append(matchQuerySlice, map[string]interface{}{
-				"match": map[string]interface{}{
-					field1[i]: keyWordFinal,
-				},
-			})
-		}
-		if isAccentMarks == false{
-			if field1[i] == "Sku_user" {
-				matchQuerySlice = append(matchQuerySlice, map[string]interface{}{
-					"match": map[string]interface{}{
-						field1[i]: keyWordFinal,
-					},
-				})
-			} else {
-				matchQuerySlice = append(matchQuerySlice, map[string]interface{}{
-					"dis_max": map[string]interface{}{
-						"queries": []interface{}{
-							map[string]interface{}{
-								"match": map[string]interface{}{
-									field1[i]: keyWordFinal,
-								},
-							},
-							map[string]interface{}{
-								"match": map[string]interface{}{
-									field1[i] + ".synonyms_v2": keyWordFinal,
-								},
-							},
-						},
-					},
-				})
-			}
-		}
-	}
-	for i := 0; i < len(field2); i++ {
-		if isAccentMarks == true{
-			tmpQuery1 := map[string]interface{}{
-				"dis_max": map[string]interface{}{
-					"queries": []interface{}{
-						map[string]interface{}{
-							"match": map[string]interface{}{
-								field2[i]: map[string]interface{}{
-									"query": keyWordFinal,
-									"boost": 6,
-								},
-							},
-						},
-						map[string]interface{}{
-							"match": map[string]interface{}{
-								field2[i] + ".with_accent_marks_v2": map[string]interface{}{
-									"boost": 12,
-									"query": keyWordFinal,
-								},
-							},
-						},
-					},
-				},
-			}
-			matchQuerySlice = append(matchQuerySlice, map[string]interface{}{
-				"bool": map[string]interface{}{
-					"should": []interface{}{
-						tmpQuery1,
-						map[string]interface{}{
-							"match_phrase": map[string]interface{}{
-								field2[i] + ".with_accent_marks_v2": map[string]interface{}{
-									"query": firstTerm,
-									"boost": 2,
-								},
-							},
-						},
-					},
-				},
-			})
-		} else {
-			matchQuerySlice = append(matchQuerySlice, map[string]interface{}{
-				"dis_max": map[string]interface{}{
-					"queries": []interface{}{
-						map[string]interface{}{
-							"match": map[string]interface{}{
-								field2[i]: map[string]interface{}{
-									"query": keyWordFinal,
-									"boost": 6,
-								},
-							},
-						},
-						map[string]interface{}{
-							"match": map[string]interface{}{
-								field2[i] + ".synonyms_v2": map[string]interface{}{
-									"query": keyWordFinal,
-									"boost": 6,
-								},
-							},
-						},
-					},
-				},
-			})
-		}
-	}
-	return matchQuerySlice
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var matchQuerySlice []interface{}
+	var keywordFormat, firstTerm string
+	var isAccentMarks bool = false
+	var isSearchInsideCategory bool = true
+	matchQuerySlice = lib.BuildInnerQueriesForSearchProductNewV8(matchQuerySlice, isSearchInsideCategory, keywordFormat, isAccentMarks, firstTerm)
+	mapB, _ := json.Marshal(matchQuerySlice)
+	fmt.Println(string(mapB))
 }
